3c5949c2a3e255110d0b63fb9f9b1e2ab2a6c24e
Kyle Lee <kyle@kylelee.dev>
2024-11-22 14:12:34 -0500

    add search feed for categories

--- src/routes/search/search-feed.tsx
+++ src/routes/search/search-feed.tsx
@@ -1,13 +1,14 @@
 import { useOutletContext } from "react-router-dom";
 import { useState } from "react";
+import { CompleteAuction } from "@/types/auction";
 function SearchPageResultsFeed() {
   const { auctions } = useOutletContext();
   const [page, setPage] = useState(0);
 
   return (
     <div>
-      {auctions.map((auction, idx) => {
-        return <div>{auction.title}</div>;
+      {auctions.map((auction: CompleteAuction) => {
+        return <div key={auction.id}>{auction.title}</div>;
       })}
     </div>
   );
--- src/routes/search/search-page.tsx
+++ src/routes/search/search-page.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useSearchParams, Outlet } from "react-router-dom";
 import { searchAuctions } from "@/utils/auctions";
-
+import { queryClient } from "@/main";
 const SearchPage = () => {
   const [searchParams] = useSearchParams();
   const category: string = searchParams.get("category") || "";
   const { isLoading, isError, data } = useQuery({
-    queryKey: ["searchResults", searchParams],
+    queryKey: ["searchResults", category],
     queryFn: () => searchAuctions(category),
   });
+  console.log("re-render");
+
+  // useEffect(() => {
+  // console/l
+  //   queryClient.invalidateQueries({ queryKey: ["searchResults"] });
+  // }, [searchParams]);
+
   if (isLoading) {
     return "Loading...";
   }
