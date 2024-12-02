c4a6c2d08ed52b5891a62af35ee9124643c99ba0
kyle <kyle@kylelee.dev>
2024-12-02 14:43:10 -0500

    use pnpm. update vite config to use dev env

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vite.dev/config/
+import * as dotenv from "dotenv";
+dotenv.config();
 const isDev = process.env.DEV === "true";
+const apiGatewayHost = isDev ? "localhost" : "api-gateway";
 export default defineConfig({
   build: {
     sourcemap: false,
@@ -16,19 +19,23 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/auctions": {
-        target: "http://localhost:42069",
+        target: `http://${apiGatewayHost}:42069`,
         changeOrigin: true,
       },
       "/api/bids": {
-        target: "http://localhost:42069",
+        target: `http://${apiGatewayHost}:42069`,
         changeOrigin: true,
       },
       "/api/watchlists": {
-        target: "http://localhost:42069",
+        target: `http://${apiGatewayHost}:42069`,
         changeOrigin: true,
       },
       "/api/categories": {
-        target: "http://localhost:42069",
+        target: `http://${apiGatewayHost}:42069`,
+        changeOrigin: true,
+      },
+      "/api/notifications": {
+        target: `http://${apiGatewayHost}:42069`,
         changeOrigin: true,
       },
     },
